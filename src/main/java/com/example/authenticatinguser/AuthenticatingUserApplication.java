686eed558fe7d3851cadab184ace8c4a7f6cd7a3
ayanfeoluwa <oladoyinoluwatomisin11@gmail.com>
2022-11-18 11:49:36 +0100

    "An authenticating user app, using ldap spring security to verify user and register user."

---
+++ src/main/java/com/example/authenticatinguser/AuthenticatingUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticatinguser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatingUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authenticatinguser/AuthenticatingUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticatinguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
